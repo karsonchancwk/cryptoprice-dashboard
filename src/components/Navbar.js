0020794b27b77032d7e443d8f84e55a1ddb30705
karsonchancwk <wingkinchancwk@gmail.com>
2023-09-23 03:07:58 +0800

    update navbar css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,14 @@ import Link from "next/link";
 export default function Navbar() {
   console.log("this is a navbar");
   return (
-    <div className="d-flex justify-between">
-      <div>Self-study Dashboard Development</div>
-      <div>
+    <div className="flex p-3 justify-between" style={{ gap: 13 }}>
+      <h1 className="flex-grow" style={{ fontSize: 30 }}>
+        Self-study Dashboard Development
+      </h1>
+      <div style={{ alignSelf: "end" }}>
         <Link href="/pricingHistory">Pricing History</Link>
+      </div>
+      <div style={{ alignSelf: "end" }}>
         <Link href="/myPortfolio">My Portfolio</Link>
       </div>
     </div>
