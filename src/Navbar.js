08d26e519c27d8d3bd4aac94266ee9aa766e3fcc
karsonchancwk <wingkinchancwk@gmail.com>
2023-09-25 02:03:28 +0800

    add chartjs, but not yet finished

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,31 @@
 import React from "react";
+import Nav from "react-bootstrap/Nav";
 
 function Navbar() {
   return (
-    <div className="flex p-3 justify-between" style={{ gap: 13 }}>
-      <h1 className="flex-grow" style={{ fontSize: 30 }}>
-        Self-study Dashboard Development
-      </h1>
-      <div style={{ alignSelf: "end" }}>
-        {/* <Link href="/pricingHistory">Pricing History</Link> */}
+    <>
+      <div style={{ gap: 13, display: "flex", padding: ".5rem 1rem" }}>
+        <h1
+          style={{
+            fontSize: 20,
+            flexGrow: 1,
+            marginTop: "auto",
+            textAlign: "start",
+          }}
+        >
+          Self-study Dashboard Development
+        </h1>
+        <Nav activeKey="/pricingHistory" className="d-flex">
+          <Nav.Item>
+            <Nav.Link href="/pricingHistory">Pricing History</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/myPortfolio">My Portfolio</Nav.Link>
+          </Nav.Item>
+        </Nav>
       </div>
-      <div style={{ alignSelf: "end" }}>
-        {/* <Link href="/myPortfolio">My Portfolio</Link> */}
-      </div>
-    </div>
+      <hr style={{ margin: "0px 0px 10px 0px" }} />
+    </>
   );
 }
 
--- src/PricingHistory.js
+++ src/PricingHistory.js
@@ -1,5 +1,44 @@
 import React from "react";
+import Papa from "papaparse";
+import csvFile from "./frontend_dataset/coin_Aave.csv";
+import { Utils } from "chart.js/auto";
+import { Line } from "react-chartjs-2";
 
 export default function PricingHistory() {
-  return <div>PricingHistory</div>;
+  const f = async () => {
+    var data;
+    const res = Papa.parse(csvFile, {
+      header: true,
+      download: true,
+      complete: function (results) {
+        console.log(results);
+        data = results.data;
+      },
+    });
+    console.log(res);
+  };
+  //
+  return (
+    <>
+      <button onClick={f}>PricingHistory</button>
+      <Line
+        datasetIdKey="id"
+        data={{
+          // labels: Utils,
+          datasets: [
+            {
+              id: 1,
+              label: "",
+              data: [5, 6, 7],
+            },
+            {
+              id: 2,
+              label: "",
+              data: [3, 2, 1],
+            },
+          ],
+        }}
+      />
+    </>
+  );
 }
