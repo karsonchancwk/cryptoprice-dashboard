d3c4f8e1d5149c9830b8cadd3993c4a0ed929503
karsonchancwk <wingkinchancwk@gmail.com>
2023-09-25 15:53:44 +0800

    fk you chartjs

--- src/PricingHistory.js
+++ src/PricingHistory.js
@@ -1,43 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Papa from "papaparse";
 import csvFile from "./frontend_dataset/coin_Aave.csv";
-import { Utils } from "chart.js/auto";
-import { Line } from "react-chartjs-2";
+import Plot from "react-plotly.js";
 
 export default function PricingHistory() {
+  const [data, setData] = useState([]);
   const f = async () => {
-    var data;
     const res = Papa.parse(csvFile, {
       header: true,
       download: true,
       complete: function (results) {
         console.log(results);
-        data = results.data;
+        if (results.data) setData(results.data);
       },
     });
-    console.log(res);
   };
   //
   return (
     <>
       <button onClick={f}>PricingHistory</button>
-      <Line
-        datasetIdKey="id"
-        data={{
-          // labels: Utils,
-          datasets: [
-            {
-              id: 1,
-              label: "",
-              data: [5, 6, 7],
-            },
-            {
-              id: 2,
-              label: "",
-              data: [3, 2, 1],
-            },
-          ],
-        }}
+      <button onClick={() => console.log(data)}>What is data</button>
+      <Plot
+        data={[
+          {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: "scatter",
+            marker: { color: "red" },
+          },
+          { type: "bar", x: [1, 2, 3], y: [2, 5, 3] },
+        ]}
+        // layout={{ width: 320, height: 240, title: "A Fancy Plot" }}
       />
     </>
   );
