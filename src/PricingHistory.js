98b77382f40476ce84d87ea409d6e2fbeb3b38ca
karsonchancwk <wingkinchancwk@gmail.com>
2023-09-26 16:22:23 +0800

    showing all coins data on graph

--- src/PricingHistory.js
+++ src/PricingHistory.js
@@ -1,47 +1,92 @@
-import React, { useState } from "react";
-import { ListGroup, Form } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { ListGroup, Form, Button, ButtonGroup } from "react-bootstrap";
 import Papa from "papaparse";
 import Plot from "react-plotly.js";
 
 import CSVs from "./frontend_dataset/CSVs";
 import AllCoins from "./frontend_dataset/summary.json";
 
+const colors = {
+  AAVE: "#8ecae6",
+  BNB: "#fb413c",
+  BTC: "#023047",
+  ADA: "#ffb703",
+  LINK: "#fb8500",
+  ATOM: "#355070",
+  CRO: "#6d597a",
+  DOGE: "#af9770",
+  EOS: "#e56b6f ",
+  ETH: "#eaac8b",
+  MIOTA: "#ef476f",
+  LTC: "#bfd166",
+  XMR: "#06d6a0 ",
+  XEM: "#118ab2",
+  DOT: "#073bac",
+  SOL: "#f79256",
+  XLM: "#fbd1a2",
+  USDT: "#7dcfb6",
+  TRX: "#00b2ca",
+  UNI: "#1d4e89",
+  USDC: "#d81159",
+  WRTC: "#8f2d56",
+  XRP: "#218380",
+};
+
 export default function PricingHistory() {
-  const [data, setData] = useState([]);
-  const [allCoins, setAllCoins] = useState();
-  const f = () => {
-    Papa.parse(CSVs["AAVE"], {
-      header: true,
-      download: true,
-      complete: function (results) {
-        console.log(results);
-        if (results.data) setData(results.data);
-      },
-    });
-  };
-  //
+  const [data, setData] = useState({});
+  const [selectedCoins, setSelectedCoins] = useState([]); // the symbols of the checked coin
+
+  useEffect(() => {
+    AllCoins.map((i) =>
+      Papa.parse(CSVs[i.Symbol], {
+        header: true,
+        download: true,
+        complete: (r) => {
+          if (r?.data) {
+            let temp = data;
+            temp[i.Symbol] = r.data;
+            setData(temp);
+          }
+        },
+      })
+    );
+  }, []);
+
   return (
     <>
-      <button onClick={f}>PricingHistory</button>
       <button onClick={() => console.log(data)}>What is data</button>
       <div className="d-flex">
-        <Plot
-          data={[
-            {
-              x: data.map((i) => i.Date),
-              y: data.map((i) => i.Close),
-              type: "scatter",
-              marker: { color: "red" },
-            },
-          ]}
-          // layout={{ width: 320, height: 240, title: "A Fancy Plot" }}
-        />
-        <Form onChange={(key) => console.log(key.target.eventkey)}>
-          <ListGroup
-            title="Select your coin"
-            // onSelect={}
-            variant="primary"
-          >
+        <div>
+          <Plot
+            data={selectedCoins.map((coin) =>
+              Object({
+                x: data[coin].map((i) => i.Date),
+                y: data[coin].map((i) => i.Close),
+                type: "scatter",
+                marker: {
+                  color: colors[Math.floor(Math.random() * colors.length)],
+                },
+              })
+            )}
+            // layout={{ width: 320, height: 240, title: "A Fancy Plot" }}
+          />
+          <ButtonGroup></ButtonGroup>
+        </div>
+        <Form
+          onChange={(c) => {
+            console.log(c.target.id); // the coin symbol
+            setSelectedCoins(
+              (p) =>
+                p.includes(c.target.id) // if the updated item is in the array
+                  ? p.filter((i) => i !== c.target.id) // remove it from the
+                  : [...p, c.target.id] // add it to the array
+            );
+          }}
+        >
+          <ListGroup title="Select your coin" variant="primary">
+            <ListGroup.Item as={Button} onClick={() => setSelectedCoins([])}>
+              Uncheck all
+            </ListGroup.Item>
             {AllCoins.map((i) => (
               <ListGroup.Item>
                 <Form.Check
@@ -49,7 +94,6 @@ export default function PricingHistory() {
                   type="checkbox"
                   id={i.Symbol}
                   label={`  ${i.Name} (${i.Symbol})`}
-                  eventKey={i.Symbol}
                 />
               </ListGroup.Item>
             ))}
